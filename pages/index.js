a14f736736d3f8d7459da349fdd902bab952a430
Bret Wadleigh <bwadleigh@marinsoftware.com>
2020-06-16 13:36:35 -0400

    Fix fetch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 // import Hero from '../components/Hero'
 // import StickyNav from '../components/StickyNav'
-// import 'isomorphic-fetch'
+import 'isomorphic-fetch'
 import { logPageView } from '../utils/analytics'
 
 export default class extends React.Component {
